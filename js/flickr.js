f70264c474ba47c6906cd488574584a8a8d84955
sanam <sanam.aghdaey@gmail.com>
2016-03-30 09:57:02 -0400

    initial git

---
+++ js/flickr.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    
+ var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+    $('form').submit(function(evt){
+        var $submitButton=$("#submit");
+        var $searchField=$('#search');
+        evt.preventDefault();
+        //hide photos below navbar 
+        $('#photos').html("");
+        $searchField.prop("disabled",true);
+        $submitButton.attr("disabled",true).val("searching..")
+        var stuff=$searchField.val();
+        var flickrOptions={
+            tags:stuff,
+            format:"json"
+        };
+        function displaySearch(data){
+            var photoHTML='<ul>';
+            $.each(data.items, function(i, photo){
+                photoHTML+='<li class="grid-25 tablet-grid-50>';
+                photoHTML+='<a href"'+ photo.link + ' " class="image">';
+                photoHTML+='<img src= "'+ photo.media.m +'"></a></li>';          
+            });
+            photoHTML+='</ul';
+            $('#searchresults').html(photoHTML); 
+            $searchField.prop("disabled",false);
+            $submitButton.attr("disabled",false).val("submit");
+        }
+         $.getJSON(flickerAPI, flickrOptions, displaySearch);
+
+    });
+    
+    
+    $('button').click(function(){
+        $("button").removeClass("selected");
+        $(this).addClass("selected");
+        var animal=$(this).text(); //will hold 'cat' when cat button is clicked
+        //we need to send this along to flickr
+        var flickrOptions={
+            tags: animal,
+            format: "json"
+        };
+        function displayPhotos(data){ //data: json data returned by jquery
+            var photoHTML='<ul>';
+            $.each(data.items, function(i, photo){
+                photoHTML+='<li class="grid-25 tablet-grid-50>';
+                photoHTML+='<a href"'+ photo.link + ' " class="image">';
+                photoHTML+='<img src= "'+ photo.media.m +'"></a></li>';
+                
+            });
+            photoHTML+='</ul';
+            $('#photos').html(photoHTML);
+        }
+        $.getJSON(flickerAPI, flickrOptions, displayPhotos);
+    });
+});
